8fdece3eab1d3f51244a11ffcae0fd93bf0ce3ed
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2024-08-20 15:46:08 +0200

    modif source download

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,15 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://staruml.io/api/download/releases-v6/StarUML%20Setup%206.0.1.exe'
+$url        = 'https://staruml.io/api/download/releases-v6/StarUML%20Setup%206.2.2.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
-
   softwareName  = 'staruml'
-
-  checksum      = '438DBB7F718EBDEF0B08791915AB2A43E0868EE650A262141A4C8422D30147DF'
+  checksum      = '22708C65298371C2DCAF84CA19616116A9AC0543DA0F7ECBD3E7425E7B731369'
   checksumType  = 'sha256'
-
   silentArgs='/S'
   validExitCodes= @(0)
 }
