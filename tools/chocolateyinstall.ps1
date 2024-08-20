0fdd28120fa8fdc1dfe2ccd6c2e9d9967562cc29
awatchanaman <94679765+awatchanaman@users.noreply.github.com>
2024-08-20 15:42:43 +0200

    modif nuspec

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://staruml.io/api/download/releases-v6/StarUML%20Setup%206.0.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'staruml'
+
+  checksum      = '438DBB7F718EBDEF0B08791915AB2A43E0868EE650A262141A4C8422D30147DF'
+  checksumType  = 'sha256'
+
+  silentArgs='/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
